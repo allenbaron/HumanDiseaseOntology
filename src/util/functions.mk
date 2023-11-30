696d5ab5ff1659023aa813ac274183767f9e92be
J. Allen Baron <jabaron.phd@gmail.com>
2023-11-30 15:34:50 -0500

    Create which_latest to detect latest OWL web version
    Works for .owl (RDF/XML) and .owl.gz files.

--- src/util/functions.mk
+++ src/util/functions.mk
@@ -15,3 +15,28 @@ define extract_versionIRI
 	sed -E 's/.*"([^"]+)".*/\1/' | \
 	sed 's|&obo;|http://purl.obolibrary.org/obo/|'
 endef
+
+# Checks for the latest version of an OBO Foundry ontology and, if newer than
+# the last check, saves it to a corresponding .version file (requires internet).
+#
+# Expects the following inputs (in order):
+# 1. <ontology>.version - should also be the rule target
+# 2. <ontology_IRI> - must be .owl or owl.gz file
+#
+# NOTE:
+# IRI must point to RDF/XML formatted file (optionally gzipped with .gz ending)
+define which_latest
+	if [[ $(2) = *".gz" ]]; then \
+		LATEST=$$(curl -sLk $(2) | gzcat | $(extract_versionIRI)) ; \
+	else \
+		LATEST=$$(curl -sLk $(2) | $(extract_versionIRI)) ; \
+	fi ; \
+	if [[ -f $(1) ]]; then \
+		SRC_VERS=$$(sed '1q' $(1)) ; \
+		if [[ $${SRC_VERS} != $${LATEST} ]]; then \
+			echo $${LATEST} > $(1) ; \
+		fi ; \
+	else \
+		echo $${LATEST} > $(1) ; \
+	fi
+endef
