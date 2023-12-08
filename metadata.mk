fb429b5d85d96bc345b3541e2b3c3b58150999a3
J. Allen Baron <jabaron.phd@gmail.com>
2023-12-08 07:33:58 -0500

    Addd more variables to metadata

--- metadata.mk
+++ metadata.mk
@@ -1,8 +1,9 @@
+# ----------------------------------------
+# ONTOLOGY METADATA
+# ----------------------------------------
+
 # Ontology ID (lowercase)
 ONTID := doid
-
-# Version of ROBOT to use
-ROBOT_VRS := 1.9.5
 
 # product to display as <ONTID>.owl
 # options: 'reasoned' or 'full' (i.e. reasoned + imports merged)
@@ -12,11 +13,37 @@ DEFAULT_PRODUCT := reasoned
 # options: 'full' or 'merged'
 FULL_SUFFIX := merged
 
-# List import ontology IDs with build rules (i.e. automated with ROBOT)
-IMPS := chebi cl eco foodon geno hp ncbitaxon ro so symp trans uberon
+# Subsets to create as products during release
+SUBSETS := DO_AGR_slim DO_FlyBase_slim DO_MGI_slim DO_GXD_slim \
+ DO_cancer_slim TopNodes_DOcancerslim DO_CFDE_slim DO_IEDB_slim DO_RAD_slim \
+ DO_rare_slim DO_infectious_disease_slim NCIthesaurus \
+ GOLD gram-negative_bacterial_infectious_disease \
+ gram-positive_bacterial_infectious_disease  \
+ sexually_transmitted_infectious_disease tick-borne_infectious_disease \
+ zoonotic_infectious_disease
+  
 
-# List import ontology IDs that are updated manually, used solely for versioning
+# ----------------------------------------
+# IMPORTS
+# ----------------------------------------
+
+# AUTOMATED IMPORTS
+# List import ontology IDs with automated build rules based on the source file
+# format, either OWL or OWL_GZ (if compressed)
+#
+# Note: Corresponding rules must be defined in the Makefile in the
+# src/ontology/imports directory
+IMP_OWL := cl eco foodon geno hp ro so symp trans uberon
+IMP_OWL_GZ := chebi ncbitaxon
+
+# MANUAL IMPORTS
+# List import ontology IDs that are updated manually (used solely for versioning)
 MANUAL_IMPS := disdriv omim_susc
+
+
+# ----------------------------------------
+# PRODUCT DIRECTORIES
+# ----------------------------------------
 
 # Release directory (and optional second directory for publishing release)
 RELEASE_DIR := src/ontology
@@ -27,3 +54,11 @@ DATASET_DIR := DOreports
 
 # Verify products in directory
 VERIFY_DIR := $(RELEASE_DIR)
+
+
+# ----------------------------------------
+# SOFTWARE DEPENDENCIES
+# ----------------------------------------
+
+# Version of ROBOT to use
+ROBOT_VRS := 1.9.5
